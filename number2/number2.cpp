4e5378d4b9ce096deb01b347c27328bf03cd9519
Dan <d.safonov.2007@mail.ru>
2026-04-03 15:31:21 +0700

    fix

--- number2/number2.cpp
+++ number2/number2.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <vector>
-void input(std::vector<int>& distance, int& planet, int& fuelTank);
+bool input(std::vector<int>& distance, int& planet, int& fuelTank);
 int resultFly(std::vector<int> distance, int planet, int fuelTank);
 int main(){
 	int planet=0, fuelTank=0;
 	std::vector<int> distance;
-	input(distance, planet, fuelTank);
-	std::cout<<resultFly(distance, planet, fuelTank);
+	if(input(distance, planet, fuelTank)){
+		std::cout<<resultFly(distance, planet, fuelTank)<<std::endl;
+	}
 	return 0;
 }
-void input(std::vector<int>& distance, int& planet, int& fuelTank){
+bool input(std::vector<int>& distance, int& planet, int& fuelTank){
+	int isDone=1;
 	if(std::cin>>planet && std::cin>>fuelTank && planet>0 && fuelTank>0){
 		for(int i=0;i<planet-1;++i){
 			int item=0;
-			std::cin>>item;
-			distance.push_back(item);
+			if(std::cin>>item && item>0){
+				distance.push_back(item);
+			}
+			else{
+				isDone=0;
+			}
 		}
 	}
 	else{
-		std::cout<<"Введите корректное число планет и объем баков";
+		isDone=0;
 	}
+	if(isDone==0){
+		std::cout<<"Введите корректное число планет, объем баков, расстояние между планетами"<<std::endl;
+	}
+	return isDone;
 }
 int resultFly(std::vector<int> distance, int planet, int fuelTank){
 	int meter=0, isDone=1;
@@ -28,20 +38,18 @@ int resultFly(std::vector<int> distance, int planet, int fuelTank){
 		if(distance[i]>fuelTank){
 			isFuel=0;
 		}
-		if(distance[i]>fuel){
+		if(distance[i]>=fuel){
 			isDone=0;
 		}
-		if(distance[i]<fuel){
+		if(distance[i]<=fuel){
 			fuel-=distance[i];
 		}
-		if(distance[i]<fuel+(fuelTank-fuel) && isDone==0 && isFuel==1){
+		if(distance[i]<=fuelTank && isDone==0 && isFuel==1){
 			++meter;
 			fuel+=(fuelTank-fuel);
 			isDone=1;
-		}
-                if(distance[i]<fuel){
 			fuel-=distance[i];
-                  }
+		}
 	}
 	return meter;
 }
