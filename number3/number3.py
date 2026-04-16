1f465e47441fae8b9e6b3adf9e1fd6b0b87dc77d
Dan <d.safonov.2007@mail.ru>
2026-04-16 23:00:15 +0700

    fix

--- number3/number3.py
+++ number3/number3.py
@@ -1,51 +1,32 @@
-import sys
-
-def input_data():
-    data = sys.stdin.read().split()
-    if not data:
-        return False, 0, []
-    try:
-        amount = int(data[0])
-    except ValueError:
-        return False, 0, []
-    if amount < 0:
-        return False, 0, []
-    numbers = []
-    idx = 1
-    for _ in range(amount):
-        if idx >= len(data):
-            return False, 0, []
-        try:
-            x = int(data[idx])
-            idx += 1
-        except ValueError:
-            return False, 0, []
-        if x <= 0:
-            return False, 0, []
-        numbers.append(x)
-    return True, amount, numbers
-
-def palindrome(amount, numbers):
-    for i in range(amount):
-        if 99 < numbers[i] < 1000:
-            unit = numbers[i] % 10
-            tens = numbers[i] % 100 - unit
-            hung = numbers[i] % 1000 - tens - unit
-            rev = (unit * 100) + tens + (hung // 100)
-            if rev == numbers[i]:
-                sys.stdout.write("Yes ")
-            else:
-                sys.stdout.write("No ")
-        else:
-            sys.stdout.write("- ")
-    sys.stdout.write("\n")
-
 def main():
-    success, amount, numbers = input_data()
-    if success:
-        palindrome(amount, numbers)
-    else:
+    try:
+        n = int(input())
+    except:
         print("Введите коректное количество чисел, числа должны быть положительные")
+        return
+    if n < 0:
+        print("Введите коректное количество чисел, числа должны быть положительные")
+        return
+    nums = []
+    for _ in range(n):
+        try:
+            x = int(input())
+            if x <= 0:
+                raise ValueError
+            nums.append(x)
+        except:
+            print("Введите коректное количество чисел, числа должны быть положительные")
+            return
+    for x in nums:
+        if 99 < x < 1000:
+            u = x % 10
+            t = x % 100 - u
+            h = x % 1000 - t - u
+            rev = u * 100 + t + h // 100
+            print("Yes " if rev == x else "No ", end="")
+        else:
+            print("- ", end="")
+    print()
 
 if __name__ == "__main__":
     main()
